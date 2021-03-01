cc0c382c89c718539c721fb190c202bfba0a4db9
Vasilios Magriplis <etameson@gmail.com>
2021-03-01 18:55:25 +1100

    Add first project

---
+++ src/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HelloWorldApp
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            print("Hello, world!");
+            print("Hello, Vasilios!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
