e08bde7594abe8a48ef9a2cdc85c3e54d3d70bc7
Vasilios Magriplis <etameson@gmail.com>
2021-03-29 20:41:45 +1100

    Import assets from NumberWizardUi and cleanup

---
+++ src/2D/BlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(nextSceneIndex);
+        else
+            SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(0);
+    }
+}
