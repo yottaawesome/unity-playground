7546033d44fe9264401fb386f4106601ae6b6074
Vasilios Magriplis <etameson@gmail.com>
2021-04-18 15:31:43 +1000

    Add audio effects to the Ball

--- src/2D/BlockBreaker/Assets/Scripts/Ball.cs
+++ src/2D/BlockBreaker/Assets/Scripts/Ball.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField]
-    float LaunchForce = 800f;
+    // Config
+    [SerializeField] float LaunchForce = 800f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float SideForce = 40f;
 
+    // State
     Paddle paddle;
     Rigidbody2D rigidBody;
     bool attached = true;
+    AudioSource audioPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioPlayer = GetComponent<AudioSource>();
         rigidBody = GetComponent<Rigidbody2D>();
         // Attach the ball to the paddle so it tracks it initially
         paddle = FindObjectOfType<Paddle>();
@@ -23,17 +28,21 @@ public class Ball : MonoBehaviour
     {
         if (attached && Input.GetMouseButtonUp(0))
         {
-            int leftOrRight = new System.Random().Next(0, 2) == 1
-                ? 1
-                : -1;
+            int leftOrRight = -1 + Random.Range(0, 2) * 2;
             transform.SetParent(null);
             rigidBody.AddForce(
                 new Vector2(
-                    leftOrRight * 40f, 
+                    leftOrRight * SideForce, 
                     transform.up.y * LaunchForce
                 )
             );
             attached = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ballSounds.Length > 0)
+            audioPlayer.PlayOneShot(ballSounds[Random.Range(0, ballSounds.Length)]);
     }
 }
