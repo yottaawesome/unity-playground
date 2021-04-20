5f871cba6255829426e652b76e71bece47547551
Vasilios Magriplis <etameson@gmail.com>
2021-04-21 08:14:52 +1000

    Track the player's score

--- src/2D/BlockBreaker/Assets/Scripts/Block.cs
+++ src/2D/BlockBreaker/Assets/Scripts/Block.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] AudioClip[] destroySounds;
+    // Config
+    [SerializeField] 
+    AudioClip[] destroySounds;
 
+    // State
     Level level;
+    GameStatus gameStatus;
 
+    #region Lifecycle
     void Start()
     {
+        // https://docs.unity3d.com/2021.1/Documentation/ScriptReference/GameObject.Find.html
         level = GameObject.FindWithTag("Level").GetComponent<Level>();
         level.AddBlock();
+        gameStatus = GameObject.FindWithTag("GameStatus").GetComponent<GameStatus>();
     }
+    #endregion
 
+    #region Events
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+    #endregion
+
+    public void DestroyBlock()
     {
         if (destroySounds.Length > 0)
         {
@@ -21,8 +36,9 @@ public class Block : MonoBehaviour
                 Camera.current.transform.position
             );
         }
-        
+
         level.RemoveBlock();
+        gameStatus.AddToScore();
         Destroy(gameObject);
     }
 }
--- src/2D/BlockBreaker/Assets/Scripts/GameStatus.cs
+++ src/2D/BlockBreaker/Assets/Scripts/GameStatus.cs
@@ -1,20 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
-    [SerializeField] [Range(0.1f, 10f)] float gameSpeed = 1f;
+    [SerializeField]
+    [Range(0.1f, 10f)] 
+    float gameSpeed = 1f;
+
+    [SerializeField]
+    int pointsPerBlockDestroyed = 1;
+
+    int currentScore = 0;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
     }
 }
