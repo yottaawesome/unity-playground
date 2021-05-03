3789da87fbcb37b9586b5a3f147f92a1b1b2de84
Vasilios Magriplis <etameson@gmail.com>
2021-05-04 07:19:52 +1000

    Set up screen boundaries

--- src/2D/LaserDefender/Assets/Scripts/Player.cs
+++ src/2D/LaserDefender/Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,10 +7,17 @@ public class Player : MonoBehaviour
     [SerializeField]
     float VerticalSpeed = 10f;
 
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetupMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -21,15 +26,33 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+        // Include the sprite bounds to prevent it clipping
+        // off at the screen extremes
+        xMin += spriteRenderer.sprite.bounds.extents.x;
+        xMax -= spriteRenderer.sprite.bounds.extents.x;
+        yMin += spriteRenderer.sprite.bounds.extents.y;
+        yMax -= spriteRenderer.sprite.bounds.extents.y;
+    }
+
     private void Move()
     {
         // Edit > Project Settings > Input Manager > Axes
-        float deltaX = Input.GetAxis("Horizontal");
-        float deltaY = Input.GetAxis("Vertical");
         // This makes it frame-rate independent
         // https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
-        float newXPos = transform.position.x + deltaX * Time.deltaTime * HorizontalSpeed;
-        float newYPos = transform.position.y + deltaY * Time.deltaTime * VerticalSpeed;
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeed;
+
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
         transform.position = new Vector3(newXPos, newYPos, transform.position.z);
     }
 }
