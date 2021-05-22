3565360dfd195cbacf2642a3420d3b59b7d14cb1
Vasilios Magriplis <etameson@gmail.com>
2021-05-22 16:41:58 +1000

    [LaserDefender] Minor refactoring to Enemy

--- src/2D/LaserDefender/Assets/Scripts/Enemy.cs
+++ src/2D/LaserDefender/Assets/Scripts/Enemy.cs
@@ -7,24 +7,20 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float health = 100;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
         if (damageDealer == null)
             return;
+        ProcessHit(damageDealer);
+    }
 
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         damageDealer.Hit();
         health -= damageDealer.GetDamage();
         if (health <= 0)
